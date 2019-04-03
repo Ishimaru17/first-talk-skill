28f15edfbd0b0ef840ebc0f061eb1359877f17ea
Ishimaru <mcampora@tssg.org>
2019-04-03 11:00:35 +0100

    try of a sequence of action

--- __init__.py
+++ __init__.py
@@ -1,16 +1,64 @@
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
 from adapt.intent import IntentBuilder
+from os.path import join, exists
+
+import subprocess
+import sys
+import os
+
+
+def read_talk(zork):
+	output = ""
+	output += zork.stdout.read(1).decode()
+	output += "____test____"
+	return output
+
+def cmd(talk, action):
+	talk.stdin.write(action.encode() + '\n')
+	talk.stdin.flush()
 
 class FirstTalk(MycroftSkill):
 	def __init__(self):
 		super(FirstTalk, self).__init__(name="FirstTalk")
+		self.talk = None
+		self.conversation = False
+		self.data = join(self._dir, 'data.txt')
 
 	@intent_handler(IntentBuilder("TalkFirstIntent").require("InitialTalk").build())
 	def handle_talk_first__intent(self, message):
+		if not self.talk:
+			self.talk = subprocess.Popen([self.data], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		cmd(self.talk, 'look')
 		self.speak_dialog('talk.first')
+		self.conversation = True
+
+	def stop_conversation(self):
+		self.playing = False
+		LOG.info('Leave')
 
 	def stop(self):
-		pass
+		if self.talk:
+			self.stop_conversation()
+
+	def converse(self, utterance):
+		if utterance:
+			utterance = utterance[0]
+			if self.conversation:
+				if "quit" in utterance or utterance == "exit":
+					#leave_zork()
+					self.stop_conversation()
+					return True
+				else: 
+					cmd(self.talk, utterance)
+					#zork_read()
+					question = read_talk(self.talk)
+					if question != "":
+						self.speak(question)
+						return True
+		return False
+
+
 
 
 def create_skill():
